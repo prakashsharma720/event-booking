e3928c45fa9e2a188f8d09bf5f32d698c60c5ea6
prakashsharma720 <34154254+prakashsharma720@users.noreply.github.com>
2024-09-06 16:43:27 +0530

    xvsds

--- coupon.php
+++ coupon.php
@@ -1,47 +1,4 @@
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "gwm_events";
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    if(!$conn)
-    {
-        die("". mysqli_error($conn));
-    }
-
-    if (isset($_GET['code'])) {
-        $couponCode = $conn->real_escape_string($_GET['code']); // Sanitize input
-    
-        // Query to select data from discounts_master table
-        $sql = "SELECT * FROM discounts_master WHERE coupon_code = '$couponCode'";
-        
-        // Execute the query
-        $result = $conn->query($sql);
-    
-        // Check if the query was successful
-        if ($result) {
-            // Check if any rows were returned
-            if (mysqli_num_rows($result) > 0) {
-                // Fetch the result
-                $row = $result->fetch_assoc();
-                echo $row['discount_value']; // Return the discount 
-            } else {
-                // Return -1 for an invalid coupon code
-                echo -1;
-            }
-        } else {
-            // Handle query error
-            echo 'Query error: ' . $conn->error;
-        }
-    } else {
-        // Return -2 if no code was provided
-        echo -2;
-    }
-    
-    // Close the connection
-    $conn->close();
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -881,7 +838,7 @@
                     currentCoupon = couponCode ;
                     // Perform AJAX request
                     $.ajax({
-                        url: 'varify_coupon.php', // Replace with the path to your PHP script
+                        url: 'verify_coupon.php', // Replace with the path to your PHP script
                         type: 'GET',
                         data: { code: couponCode },
                         success: function(response) {
