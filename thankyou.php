a8bbff624e5888915c9a007dca76511e3e6be3f9
prakashsharma720 <34154254+prakashsharma720@users.noreply.github.com>
2024-11-15 17:40:36 +0530

    Update thankyou.php

--- thankyou.php
+++ thankyou.php
@@ -561,6 +561,8 @@ if (!isset($_SESSION['user_id'])) {
   
 
 </body>
+ <script src="https://moerp.modevcloud.com/assets/plugins/jquery/jquery.min.js"></script>
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
       <!-- Download Ticket Script -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
