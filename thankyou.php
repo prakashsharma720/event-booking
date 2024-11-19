71ff38929dbfa8356ca91f590019651e9c36770e
prakashsharma720 <34154254+prakashsharma720@users.noreply.github.com>
2024-11-19 10:26:41 +0530

    asd

--- thankyou.php
+++ thankyou.php
@@ -365,7 +365,7 @@ if (!isset($_SESSION['user_id'])) {
             <a href="https://portal.glowupwithmanisha.com/" class="btn"> Profile</a>
         </div>
         <div class="btn-container" style="padding-left: 30px;">
-            <a href="login.php" style="color:#fff;text-decoration:none;"> <img src="image/logout.png" class="logout_icon"> Logout</a>
+            <a href="logout.php" style="color:#fff;text-decoration:none;"> <img src="image/logout.png" class="logout_icon"> Logout</a>
         </div>
 
 
